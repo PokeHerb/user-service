1d8c8c0304ba7b78fa497ec729526d188cb5362a
jinnk0 <jin8073@gmail.com>
2025-11-13 12:34:59 +0900

    RabbitMQ 설정 예시

---
+++ src/main/java/org/pokeherb/hubservice/infrastructure/config/RabbitProductProperties.java
@@ -0,0 +1,12 @@
+package org.pokeherb.hubservice.infrastructure.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = "rabbit.product")
+public class RabbitProductProperties {
+    private String exchange;
+    private String queue;
+    private String routingKey;
+}
