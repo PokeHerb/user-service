b5c05db8cf81a785ba83b7a42f79b9d062522afa
Yejin-source <yejin3831@naver.com>
2025-11-17 17:28:46 +0900

    add: 초기 세팅 수정

--- src/main/java/org/pokeherb/userservice/infrastructure/client/ExampleClient.java
+++
@@ -1,7 +0,0 @@
-package org.pokeherb.userservice.infrastructure.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient("example-service")
-public interface ExampleClient {
-}
--- src/main/java/org/pokeherb/userservice/infrastructure/config/FeignConfig.java
+++
@@ -1,9 +0,0 @@
-package org.pokeherb.userservice.infrastructure.config;
-
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableFeignClients("org.pokeherb.userservice")
-public class FeignConfig {
-}
